27d677fab004dd23cd246eb48a64601347a0f6fe
Tayoid <idowutemitayoisaiah@gmail.com>
2024-07-11 14:45:50 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  // These paths are just examples, customize them to match your project structure
+  
+  content: ["./build/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
